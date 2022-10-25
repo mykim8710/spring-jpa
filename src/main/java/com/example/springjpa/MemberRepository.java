0a1ea315828a238e6bbfecaacf56d6457c701a5a
mykim <mykim8710@gmail.com>
2022-10-25 23:55:30 +0900

    [ Study ] Spring - JPA - View 환경 설정 및 테스트 -  JPA와 DB 설정 : application.yml - 동작확인 : entity, repository -> TestCode로 검증

---
+++ src/main/java/com/example/springjpa/MemberRepository.java
@@ -0,0 +1,26 @@
+package com.example.springjpa;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+     private final EntityManager em;
+//    @PersistenceContext
+//    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member findById(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/com/example/springjpa/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.springjpa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional  // jpa의 변화(insert, update, delete)는 트랙잭션 안에서 이루어져야한다.'
+    @Rollback(false)
+    public void saveTest() {
+        // given
+        Member member = new Member("memberA");
+
+        // when
+        Long savedMemberId = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMemberId);
+        assertThat(findMember.getId()).isEqualTo(savedMemberId);
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);    // == jpa 엔티티 동일성 보장
+    }
+
+}
