454ba55db5a967a9ba6ab75e4788e100c1a6835e
mykim <mykim8710@gmail.com>
2022-11-07 22:01:20 +0900

    [ edit ] Spring - JPA - Sign in(spring security) 추후 구현으로 인한 Member entity 객체수정

--- src/main/java/com/example/springjpa/member/Member.java
+++ src/main/java/com/example/springjpa/member/Member.java
@@ -1,71 +1,25 @@
 package com.example.springjpa.member;
 
-import com.example.springjpa.config.security.dto.ResponseSignInMemberDto;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
-public class Member implements UserDetails {
+public class Member {
     @Id
     @GeneratedValue
     private Long id;
     private String username;
-    private String password;
-    private String auth;
-
 
     public Member() {
     }
 
     public Member(String username) {
         this.username = username;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
-
-    public ResponseSignInMemberDto toResponseSignInMemberDto() {
-        return ResponseSignInMemberDto.builder()
-                .id(id)
-                .username(username)
-                .auth(auth)
-                .build();
     }
 }
